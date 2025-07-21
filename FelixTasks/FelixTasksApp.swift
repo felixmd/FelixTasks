1647f94001280988e36ddec756c137f9bda5fa5a
Felix Devasia <felixmd@gmail.com>
2025-07-21 07:33:59 -0400

    Initial Commit

---
+++ FelixTasks/FelixTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FelixTasksApp.swift
+//  FelixTasks
+//
+//  Created by Felix Devasia on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FelixTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
